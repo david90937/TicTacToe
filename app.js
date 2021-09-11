7217b656a09eaee9968c3afec77a8fd7aa943d88
David Robinette <drobin4@gmail.com>
2021-09-11 18:42:02 -0500

    refactored winningCondition into gameWon, added declareWinner function.

--- app.js
+++ app.js
@@ -3,34 +3,28 @@ let cells = document.querySelectorAll('td');
 let player = "X";
 let xMoves = [];
 let oMoves = [];
+let winningStates = [
+    ["box1", "box2", "box3"], 
+    ["box4", "box5", "box6"], 
+    ["box7", "box8", "box9"], 
+    ["box1", "box5", "box9"], 
+    ["box1", "box4", "box7"], 
+    ["box2", "box5", "box8"], 
+    ["box3", "box6", "box9"], 
+    ["box3", "box5", "box7"]
+    ];
 
 for (i=0; i < cells.length; i++) {
     cells[i].addEventListener('click', onClick)
 }
 
 function onClick() {
-    let wincon = win_condition();
-    if (wincon == "x wins"){
-        alert("X wins!");
-        reset();
-        return;
-    }
-    if (wincon == "o wins"){
-        alert("O wins!");
-        reset();
-        return;
-    }
-    // Known bug - sometimes the final X or O is not added to the board. Sometimes it is. Only seems to happen on draw condition. 
-    if (wincon == "draw"){
-        alert("Draw!");
-        reset();
-        player = ""; // added to prevent the board from registering final click from previous game on board after reset
-    }
     if (event.target.textContent != ""){
         return;
     }
     event.target.textContent = player;
     change_player();
+    declareWinner();
 }
 
 function change_player() {
@@ -43,63 +37,36 @@ function change_player() {
         player = "X";
     }
 }
- 
-function win_condition() {
-    // X win conditions
-    if (xMoves.includes("box1") && xMoves.includes("box2") && xMoves.includes("box3")) {
-        return "x wins";
-    }
-    else if (xMoves.includes("box4") && xMoves.includes("box5") && xMoves.includes("box6")) {
-        return "x wins";
-    }
-    else if (xMoves.includes("box7") && xMoves.includes("box8") && xMoves.includes("box9")) {
-        return "x wins";
-    }
-    else if (xMoves.includes("box1") && xMoves.includes("box5") && xMoves.includes("box9")) {
-        return "x wins";
-    }
-    else if (xMoves.includes("box1") && xMoves.includes("box4") && xMoves.includes("box7")) {
-        return "x wins";
-    }
-    else if (xMoves.includes("box2") && xMoves.includes("box5") && xMoves.includes("box8")) {
-        return "x wins";
-    }
-    else if (xMoves.includes("box3") && xMoves.includes("box6") && xMoves.includes("box9")) {
-        return "x wins";
-    }
-    else if (xMoves.includes("box3") && xMoves.includes("box5") && xMoves.includes("box7")) {
-        return "x wins";
-    }
 
-    // O win conditions
-    else if (oMoves.includes("box1") && oMoves.includes("box2") && oMoves.includes("box3")) {
-        return "o wins";
+function gameWon(moves){
+    moves = moves.sort();
+    for (let i = 0; i < winningStates.length; i++) {    
+        let a = moves.includes(winningStates[i][0]);
+        let b = moves.includes(winningStates[i][1]);
+        let c = moves.includes(winningStates[i][2]);
+
+        if (a && b && c){
+            return true;
+        }
     }
-    else if (oMoves.includes("box4") && oMoves.includes("box5") && oMoves.includes("box6")) {
-        return "o wins";
+}
+
+function declareWinner(){
+    if (gameWon(xMoves)){
+        alert("X wins!");
+        reset();
+        return;
     }
-    else if (oMoves.includes("box7") && oMoves.includes("box8") && oMoves.includes("box9")) {
-        return "o wins";
+    if (gameWon(oMoves)){
+        alert("O wins!");
+        reset();
+        return;
     }
-    else if (oMoves.includes("box1") && oMoves.includes("box5") && oMoves.includes("box9")) {
-        return "o wins";
-    }
-    else if (oMoves.includes("box1") && oMoves.includes("box4") && oMoves.includes("box7")) {
-        return "o wins";
-    }
-    else if (oMoves.includes("box2") && oMoves.includes("box5") && oMoves.includes("box8")) {
-        return "o wins";
-    }
-    else if (oMoves.includes("box3") && oMoves.includes("box6") && oMoves.includes("box9")) {
-        return "o wins";
-    }
-    else if (oMoves.includes("box3") && oMoves.includes("box5") && oMoves.includes("box7")) {
-        return "o wins";
-    }
-    // If no winnner, game is a draw.
-    else if ((xMoves + oMoves).length == 43){
-        console.log((xMoves + oMoves).length);
-        return "draw";
+        // If no winnner, game is a draw.
+    else if ((xMoves.length + oMoves.length)== 9){ // checks to see if all 9 squares are filled
+        alert("Draw!");
+        reset();
+        return;
     }
 }
 
